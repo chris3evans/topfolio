ed040896d639999e9012de05dcbf241b650b5fe3
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-26 15:49:32 +0000

    fix(apiservice): added token to api service request

--- apps/topfolio/src/utils/fetcher.ts
+++ apps/topfolio/src/utils/fetcher.ts
@@ -3,14 +3,19 @@ import { User } from '@topfolio/api-interfaces';
 export const fetcher = async (
   url: string,
   method: string,
-  body?: User | object
+  body?: User | object,
+  token?: string
 ): Promise<User | object> => {
   try {
     const res = await fetch(url, {
       method: `${method}`,
       credentials: 'include',
       mode: 'cors',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        //prettier-ignore
+        'Authentication': `Bearer ${token}`,
+      },
       body: JSON.stringify(body),
     });
     return await res.json();
