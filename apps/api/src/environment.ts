34037f46ba7bc99dc6d46b4d6b4a9dab0d19e771
noah-sinnott <noah.sinnott123@gmail.com>
2022-11-28 09:48:35 +0000

    changed attempt 2

--- apps/api/src/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    DB_URI: "mongodb+srv://TopfolioTeam:oppD3HbV8bmJaMAr@cluster0.iitg10g.mongodb.net/?retryWrites=true&w=majority",
-    AUTH0_DOMAIN: 'dev-onbdt8c3z3jnm7rk.us.auth0.com',
-    AUTH0_AUDIENCE: 'https://hello-world.example.com',
-};
