c4d00cb9884e30f1ebc05a1966176ac34feb12fa
noah-sinnott <noah.sinnott123@gmail.com>
2022-11-28 09:29:06 +0000

    tests

--- apps/api/src/models/dbConnection.ts
+++ apps/api/src/models/dbConnection.ts
@@ -1,4 +1,4 @@
-import { environment } from "../environments/environment.prod";
+import { environment } from "../environments/environment";
 const uri = environment.DB_URI
 import { connect } from "mongoose";
 
