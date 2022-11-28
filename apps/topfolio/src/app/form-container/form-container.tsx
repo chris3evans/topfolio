927bcc6fdaacbc27d0e20d696de319287d57b361
Chenhao Hu <huc17@cardiff.ac.uk>
2022-11-28 19:48:40 +0000

    feat: a simple form done

--- apps/topfolio/src/app/form-container/form-container.tsx
+++ apps/topfolio/src/app/form-container/form-container.tsx
@@ -1,13 +1,15 @@
 import styles from './form-container.module.css';
 import FormWorkExperience from '../form-work-experience/form-work-experience';
+import FormProjects from '../form-projects/form-projects';
 
 /* eslint-disable-next-line */
-export interface FormContainerProps {}
+export interface FormContainerProps { }
 
 export function FormContainer(props: FormContainerProps) {
   return (
     <div className={styles['form-container']}>
-      <FormWorkExperience></FormWorkExperience>
+      <FormProjects></FormProjects>
+      {/* <FormWorkExperience></FormWorkExperience> */}
     </div>
   );
 }
--- apps/topfolio/src/app/form-projects/form-projects.tsx
+++ apps/topfolio/src/app/form-projects/form-projects.tsx
@@ -1,13 +1,119 @@
 import styles from './form-projects.module.css';
+import muiStyles from './styles-projects';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { updateUser } from '../../utils/ApiService';
+import { UserContext } from '../../utils/UserContext';
+import { useContext } from 'react';
 
-/* eslint-disable-next-line */
-export interface FormProjectsProps {}
-
+export interface FormProjectsProps { }
 export function FormProjects(props: FormProjectsProps) {
+  const context = useContext(UserContext);
+
+  const formSubmitHandler = async function (event: any) {
+    try {
+      event.preventDefault();
+
+      const formData = {
+        company_name: event.target.projectName.value,
+        description: event.target.description.value,
+        start_date: event.target.gitUrl.value,
+        end_date: event.target.appUrl.value,
+      };
+
+      const response = await updateUser(formData, '');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to FormProjects!</h1>
-    </div>
+    <Box sx={muiStyles.form}>
+      <Typography align="center" sx={muiStyles.formTitle} variant="h2">
+        My Projects
+      </Typography>
+      {/* name: string;
+  images: string[];
+  description: string;
+  github_url: string;
+  app_url: string; */}
+      <form onSubmit={formSubmitHandler} className={styles['form-we']}>
+        <Box sx={muiStyles.formFields}>
+          <Box sx={muiStyles.projectField}>
+            <FormControl fullWidth={true}>
+              <InputLabel htmlFor="project-name">Project Name:</InputLabel>
+              <Input
+                type="text"
+                required
+                id="project name"
+                name="projectName"
+              ></Input>
+            </FormControl>
+
+          </Box>
+
+
+
+          <Box sx={muiStyles.descriptionField}>
+            <FormControl fullWidth={true}>
+              <InputLabel htmlFor="description">Description:</InputLabel>
+              <Input
+                type="text"
+                required
+                id="description"
+                name="description"
+                multiline={true}
+              ></Input>
+            </FormControl>
+          </Box>
+
+          <Box sx={muiStyles.gitUrlField}>
+            <FormControl fullWidth={true}>
+
+
+              <InputLabel htmlFor="git-url">Link to your Github Repo:</InputLabel>
+              <Input
+                type="text"
+                required
+                id="git url"
+                name="gitUrl"
+                multiline={true}
+              ></Input>
+            </FormControl>
+          </Box>
+
+          <Box sx={muiStyles.appUrlField}>
+            <FormControl fullWidth={true}>
+              <InputLabel htmlFor="app-url">Link to the App:</InputLabel>
+              <Input
+                type="text"
+                required
+                id="app url"
+                name="appUrl"
+                multiline={true}
+              ></Input>
+            </FormControl>
+          </Box>
+          <Button variant="contained" component="label">
+            Upload Picture
+            <input hidden accept="image/*" multiple type="file" />
+          </Button>
+          <Button sx={muiStyles.saveButton} type="submit" variant="contained">
+            Save
+          </Button>
+        </Box>
+
+      </form>
+    </Box>
   );
 }
 
