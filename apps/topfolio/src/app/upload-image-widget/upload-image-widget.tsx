cdf15a9fdc9f7dd5e18fb8d15aed91140a07ec63
Chenhao Hu <huc17@cardiff.ac.uk>
2022-11-30 12:47:02 +0000

    feat: widget md done

--- apps/topfolio/src/app/upload-image-widget/upload-image-widget.tsx
+++ apps/topfolio/src/app/upload-image-widget/upload-image-widget.tsx
@@ -1,3 +1,30 @@
+//To use this widget
+//1: Import this widget at the top of your file, like:
+// import UploadImageWidget from '../upload-image-widget/upload-image-widget';
+//2: Use it as a component on your page, like:
+// <UploadImageWidget callback={getUploadedImage} />
+//3: By calling the getUploadedImage function, you will get an img object like this for every time you upload a picture:
+//{ url: string, id: string }
+//4: You can do whatever you want with it. A way to use it is to push these objects into an array state 
+// const [imgArray, setImageArray] = useState<{ url: string, id: string }[]>([]);
+//const getUploadedImage = (img: { url: string, id: string }) => {
+//   setImageArray(array => {
+//     return [
+//       ...array,
+//       img
+//     ]
+//   });
+// }
+//Or if you just want to use one img:
+// const [img, setImage] = useState<string>('');
+// const getUploadedImage = (img: { url: string, id: string }) => {
+//     setImageArray(img)
+//     };
+//img.url is what you need to display img directly. 
+//img.id is for using cloudinary after-processing pipeline.
+
+
+
 import { useEffect, useRef } from "react";
 import Button from '@mui/material/Button';
 /* eslint-disable-next-line */
@@ -8,7 +35,6 @@ export interface UploadImageWidgetProps {
 export function UploadImageWidget(props: UploadImageWidgetProps) {
   const CloudinaryRef = useRef<any>();
   const widgetRef = useRef<any>();
-  console.log(CloudinaryRef.current, "CloudinaryRef.current")
 
   useEffect(() => {
     CloudinaryRef.current = window.cloudinary;
@@ -17,9 +43,7 @@ export function UploadImageWidget(props: UploadImageWidgetProps) {
         cloudName: "divt6a0ys",
         uploadPreset: "gqo4iyb7"
       }, function (error: any, result: any) {
-        // console.log(error, result, "cloudinary e@r")
         if (result.event === 'success') {
-          console.log(result, "result obj")
           const imgInfo = {
             url: result.info.secure_url,
             id: result.info.public_id
@@ -33,11 +57,9 @@ export function UploadImageWidget(props: UploadImageWidgetProps) {
 
   }, [])
   return (
-    // <button onClick={() => widgetRef.current.open()}></button>
     <Button variant="contained" component="label" onClick={() => widgetRef.current.open()}>
       Upload Picture
     </Button>
-
   );
 }
 
