f80b040c5df8b06e26047d517e70623133796eb7
chris3evans <chris3evans.ce@gmail.com>
2022-11-30 09:51:57 +0000

    feat(listworkexperience): form and list now render conditionally within the form container

--- apps/topfolio/src/app/form-container/form-container.tsx
+++ apps/topfolio/src/app/form-container/form-container.tsx
@@ -3,6 +3,9 @@ import FormWorkExperience from '../form-work-experience/form-work-experience';
 import FormProjects from '../form-projects/form-projects';
 import InfoAboutMe from '../info-about-me/info-about-me';
 import ContactMeForm from '../contact-me-form/contact-me-form';
+import { useContext } from 'react';
+import { UserContext } from '../../utils/UserContext';
+import ListWorkExperience from '../list-work-experience/list-work-experience';
 
 /* eslint-disable-next-line */
 export interface FormContainerProps {
@@ -11,11 +14,18 @@ export interface FormContainerProps {
 }
 
 export function FormContainer(props: FormContainerProps) {
+  const userContext = useContext(UserContext);
+
   return (
     <div className={styles['form-container']}>
       {props.sectionName == 'about-me' ? <InfoAboutMe></InfoAboutMe> : ''}
       {props.sectionName == 'work-experience' ? (
-        <FormWorkExperience token={props.token}></FormWorkExperience>
+        /* @ts-ignore */
+        userContext.userDetails.portfolio.work_history ? (
+          <ListWorkExperience></ListWorkExperience>
+        ) : (
+          <FormWorkExperience token={props.token}></FormWorkExperience>
+        )
       ) : (
         ''
       )}
