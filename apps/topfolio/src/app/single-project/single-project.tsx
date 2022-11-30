c1e2b9aaf01c5fa9c40e5f9bdd7bf3b290b30136
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-30 12:41:02 +0000

    feat(project/component): finished up laptop/project animation

--- apps/topfolio/src/app/single-project/single-project.tsx
+++ apps/topfolio/src/app/single-project/single-project.tsx
@@ -1,28 +1,69 @@
-import { MyProjects, User } from '@topfolio/api-interfaces';
+import { MyProjects } from '@topfolio/api-interfaces';
 import styles from './single-project.module.css';
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
+import { FiGithub } from 'react-icons/fi';
+import { FaAppStore } from 'react-icons/fa';
 
 /* eslint-disable-next-line */
 export interface SingleProjectProps {
   project: MyProjects;
 }
-
 export function SingleProject(props: SingleProjectProps) {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
   return (
     <div className={styles['projects-cont']} id="project-div">
-      <div className={styles['laptop-bg']}>
-        <div
+      <motion.div
+        className={styles['laptop-bg']}
+        animate={
+          isInView
+            ? {
+                scale: 1,
+                transition: {
+                  type: 'tween',
+                  delay: 1.5,
+                  duration: 1,
+                },
+              }
+            : { scale: 0.8, transition: { delay: 0.5 } }
+        }
+      >
+        <motion.div
           className={styles['laptop-content-bg']}
           style={{ backgroundImage: `url(${props.project.images[0]})` }}
-        ></div>
-        <img src="../../assets/laptop.png" alt="laptopFrame" />
-
-        <div className={styles['infos']}>
-          <p>{props.project.name}</p>
+        ></motion.div>
+        <motion.img src="../../assets/laptop.png" alt="laptopFrame" />
+        <motion.div
+          className={styles['infos']}
+          ref={ref}
+          animate={
+            isInView
+              ? {
+                  opacity: 1,
+                  transition: {
+                    type: 'tween',
+                    delay: 1.5,
+                    duration: 1,
+                  },
+                }
+              : { opacity: 0, transition: { delay: 0.5 } }
+          }
+        >
           <p>{props.project.description}</p>
-          <a href={props.project.github_url}>Github</a>
-          <a href={props.project.app_url}>App</a>
-        </div>
-      </div>
+          <div className={styles['links']}>
+            <a href={props.project.github_url}>
+              <FiGithub />
+              Github
+            </a>
+            <a href={props.project.app_url}>
+              <FaAppStore />
+              App
+            </a>
+          </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
