ac12882fd420be541a6e7a7dcd6c7021666874ed
Chenhao Hu <huc17@cardiff.ac.uk>
2022-12-02 18:06:19 +0000

    feat: ItemProjects test done

--- apps/topfolio/src/app/list-projects/list-projects.spec.tsx
+++ apps/topfolio/src/app/list-projects/list-projects.spec.tsx
@@ -1,10 +1,33 @@
 import { render } from '@testing-library/react';
-
+import { expect, describe, it } from '@jest/globals';
+import { screen } from '@testing-library/react';
 import ListProjects from './list-projects';
+import { mockUserState } from '../mockUser'
+import { UserContext } from '../../utils/UserContext';
+
+const userDetails = mockUserState;
+const setUser = () => { };
+
 
 describe('ListProjects', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<ListProjects />);
+  it('should render successfully when context is empty', () => {
+    const { baseElement } = render(<ListProjects token={'testToken'} />);
     expect(baseElement).toBeTruthy();
   });
+
+  it('should render a FormProjects component when context is empty', async () => {
+    const { baseElement } = render(<ListProjects token={'testToken'} />);
+    const aFormProjectsLabel = await screen.findAllByTestId('projectNameLabel')
+    expect(aFormProjectsLabel).toBeTruthy();
+  });
+
+  it('should render successfully with context provided userDetails', async () => {
+    const { baseElement } = render(
+      <UserContext.Provider value={{ userDetails, setUser }}>
+        <ListProjects token={'testToken'} />
+      </UserContext.Provider>
+    );
+    const aFormProjectsLabel = await screen.findAllByTestId('projectNameLabel')
+    expect(baseElement).toBeTruthy();
+  })
 });
